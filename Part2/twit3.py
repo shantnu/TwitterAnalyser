3cbb5e8cf8eb46aa3cf1a1643d5838c8d02352d2
Shantnu Tiwari <a@a.com>
2015-08-21 14:18:54 +0100

    Renaming & moving files arounf

--- Part2/twit3.py
+++ Part2/twit3.py
@@ -6,51 +6,11 @@ import pdb
 import json
 from collections import Counter
 
-langs = \
-    {'ar': 'Arabic',
-     'bg': 'Bulgarian',
-     'ca': 'Catalan',
-     'cs': 'Czech',
-     'da': 'Danish',
-     'de': 'German',
-     'el': 'Greek',
-     'en': 'English',
-     'es': 'Spanish',
-     'et': 'Estonian',
-     'fa': 'Persian',
-     'fi': 'Finnish',
-     'fr': 'French',
-     'hi': 'Hindi',
-     'hr': 'Croatian',
-     'hu': 'Hungarian',
-     'id': 'Indonesian',
-     'is': 'Icelandic',
-     'it': 'Italian',
-     'iw': 'Hebrew',
-     'ja': 'Japanese',
-     'ko': 'Korean',
-     'lt': 'Lithuanian',
-     'lv': 'Latvian',
-     'ms': 'Malay',
-     'nl': 'Dutch',
-     'no': 'Norwegian',
-     'pl': 'Polish',
-     'pt': 'Portuguese',
-     'ro': 'Romanian',
-     'ru': 'Russian',
-     'sk': 'Slovak',
-     'sl': 'Slovenian',
-     'sr': 'Serbian',
-     'sv': 'Swedish',
-     'th': 'Thai',
-     'tl': 'Filipino',
-     'tr': 'Turkish',
-     'uk': 'Ukrainian',
-     'ur': 'Urdu',
-     'vi': 'Vietnamese',
-     'zh_CN': 'Chinese (simplified)',
-     'zh_TW': 'Chinese (traditional)'
-     }
+langs =   {'ar': 'Arabic',  'bg': 'Bulgarian', 'ca': 'Catalan','cs': 'Czech','da': 'Danish','de': 'German','el': 'Greek','en': 'English','es': 'Spanish','et': 'Estonian',\
+           'fa': 'Persian','fi': 'Finnish','fr': 'French','hi': 'Hindi','hr': 'Croatian','hu': 'Hungarian','id': 'Indonesian','is': 'Icelandic','it': 'Italian','iw': 'Hebrew',\
+           'ja': 'Japanese','ko': 'Korean','lt': 'Lithuanian','lv': 'Latvian','ms': 'Malay','nl': 'Dutch','no': 'Norwegian','pl': 'Polish','pt': 'Portuguese','ro': 'Romanian',\
+           'ru': 'Russian','sk': 'Slovak','sl': 'Slovenian','sr': 'Serbian','sv': 'Swedish','th': 'Thai','tl': 'Filipino','tr': 'Turkish','uk': 'Ukrainian','ur': 'Urdu',\
+           'vi': 'Vietnamese','zh_CN': 'Chinese (simplified)','zh_TW': 'Chinese (traditional)'}
 
 
 class twitter_listener(StreamListener):
@@ -64,12 +24,12 @@ class twitter_listener(StreamListener):
         try:
             json_data = json.loads(data)
             print(json_data["text"])
-            self.languages.append(langs[json_data["lang"]])
-            print(Counter(self.languages))
+            self.languages.append(langs[json_data["lang"]])            
 
             self.counter += 1
             if self.counter == self.num_tweets_to_grab:
                 print(self.languages)
+                print(Counter(self.languages))
                 return False
 
             return True
